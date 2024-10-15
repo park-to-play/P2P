8e75f1d1fa5f9770e566b96436082025dc1fef93
DongOk <hcan159@naver.com>
2024-10-15 21:08:05 +0900

    @add search api

--- p2p/location/views.py
+++ p2p/location/views.py
@@ -3,11 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import SearchLocation
 from .serializers import SearchLocationSerializer
+from .Search import Search
 
 class SearchLocationListView(APIView):
     def post(self, request):
         serializer = SearchLocationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response("serializer.data", status=status.HTTP_201_CREATED)  
+            location = serializer.validated_data.get('location')
+            result=Search(location)
+            return Response(result, status=status.HTTP_201_CREATED)  
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
